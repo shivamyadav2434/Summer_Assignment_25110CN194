95b136f45b457c42f26422376ceaae8408c8c360
shivamyadav2434 <yadavshivam20122006@gmail.com>
2026-06-03 11:05:48 +0530

    Add files via upload

---
+++ day2/Q6.c
@@ -0,0 +1,19 @@
+// write a program to reverse a number
+#include <stdio.h>
+
+int main()
+{
+    int n, rev = 0;
+
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+
+    printf("%d", rev);
+
+    return 0;
+}
