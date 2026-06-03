129d2af58c6295391d6bf93485f0f5a18dc6843e
shivamyadav2434 <yadavshivam20122006@gmail.com>
2026-06-03 10:43:32 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,19 @@
+// write a program to find sum of digit of a number
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
---
+++ Q8.c
@@ -0,0 +1,24 @@
+// write a program to check weather a number is palindrome
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, rev = 0;
+
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+
+    if (temp == rev)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
