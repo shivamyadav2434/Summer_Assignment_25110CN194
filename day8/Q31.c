408ba8e6caabce07b945c276c3c300928f0cc80f
shivamyadav2434 <yadavshivam20122006@gmail.com>
2026-06-08 21:10:48 +0530

    Add files via upload

---
+++ day8/Q31.c
@@ -0,0 +1,21 @@
+//write a program to print character triangle
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 0; j < i; j++)
+        {
+            printf("%c ", 'A' + j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
