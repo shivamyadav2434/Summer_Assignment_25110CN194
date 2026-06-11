2b040dabd1563cc6cef6257ec812e5eeb102e284
shivamyadav2434 <yadavshivam20122006@gmail.com>
2026-06-11 20:26:27 +0530

    Add files via upload

---
+++ day11/Q41.c
@@ -0,0 +1,19 @@
+// write a program to write function to find sum of two numbers
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Sum = %d", sum(x, y));
+
+    return 0;
+}
---
+++ day11/Q42.c
@@ -0,0 +1,22 @@
+// write a program to write function to find maximum
+#include <stdio.h>
+
+int maximum(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Maximum = %d", maximum(x, y));
+
+    return 0;
+}
